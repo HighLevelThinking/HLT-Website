82c40ccac5fd76b1809db0a4fa8216e442e21973
MrPeep0517 <mrpeep0517@gmail.com>
2025-11-07 10:14:07 -0800

    i dont know what i did

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -9,6 +9,7 @@ function Header() {
                 <li><button onClick={() => window.location.href = 'https://www.google.com'}>About</button></li>
                 <li><button onClick={() => window.location.href = 'https://www.google.com'}>Products</button></li>
                 <li><button onClick={() => window.location.href = 'https://www.google.com'}>Software</button></li>
+                <li><button onClick={() => window.location.href = 'https://www.google.com'}>Cart</button></li>
             </ul>
         </header>
     );
